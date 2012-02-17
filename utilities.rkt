54c191585d491f41d2bf99db15852f6799b1132d
Daniel King <danking@ccs.neu.edu>
2012-02-17 14:21:42 -0500

    make utilities a proper #lang module by parametrize over some bindings

--- utilities.rkt
+++ utilities.rkt
@@ -1,24 +1,31 @@
-;; bpset->fv-hash : [SetOf BP] [FV FV -> FV] -> [Hash AState FV]
-;; this needs to be in the scope that contains BP, note that join must be a
-;; join on flow values not on states
-(define (bpset->fv-hash bps join)
+#lang racket
+(require "cfa2.rkt")
+(provide (all-defined-out))
+
+;; bpset->fv-hash : [SetOf BP]
+;;                  [FState -> AState FV]
+;;                  [FV FV -> FV]
+;;                  ->
+;;                  [Hash AState FV]
+;; the splitter should return two values when given a flow-state, the abstract
+;; state and the flow value.
+(define (bpset->fv-hash bpset splitter join)
   (for/fold ((hsh (hash)))
       ((bp (in-set bpset)))
     (match-define (BP openfstate fstate) bp)
     (define (add-to-hash fstate hsh)
-      (match-define (flow-state astate fv) fstate)
+      (define-values (astate fv) (splitter fstate))
       (hash-set hsh astate (join (hash-ref hsh astate +inf.0)
                                  fv)))
 
     (add-to-hash openfstate (add-to-hash fstate hsh))))
 
 ;; sorts by UID
-(define (fv-hash->sorted-list hsh)
+(define (fv-hash->sorted-list hsh get-uid)
   (sort (hash->list hsh)
         (lambda (a b)
-          (match-define (abstract-state (node uid-a) _ _) (car a))
-          (match-define (abstract-state (node uid-b) _ _) (car b))
-          (< uid-a uid-b))))
+          (< (get-uid a)
+             (get-uid b)))))
 
 ;; set-add* : [Set X] X ... -> [Set X]
 (define (set-add* s . vs)
