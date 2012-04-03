392dc45809ded4937307c7fb861ffef2f0bb3ef5
Daniel King <danking@ccs.neu.edu>
2012-04-03 15:03:33 -0400

    add optimized implementations for Summaries and Callers sets

--- cfa2.rkt
+++ cfa2.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require ; "../racket-utils/set-utilities.rkt"
-         "../racket-utils/similar-sets.rkt")
+         "../racket-utils/similar-sets.rkt"
+         "../racket-utils/option.rkt"
+         (prefix-in basic- racket/set))
 (provide FlowAnalysis BP CFA2)
 
 (define-struct BP (open node) #:transparent)
@@ -59,76 +61,116 @@
     (match-define (BP open node) bp)
     (+ (state-hash-code open) (state-hash-code node)))
 
-  (define empty-bp-set (set join state-equal? bp-similar? bp-hash-code))
+  (define empty-W/Paths-set (set bp-join equal? bp-similar? bp-hash-code))
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Common to Callers and Summaries
+  (define (basic-set-union/singletons-are-sets v1 v2)
+    (match (list v1 v2)
+      ((list (BP _ _) (BP _ _)) (basic-set v1 v2))
+      ((or (list (and bp (BP _ _)) a-basic-set)
+           (list a-basic-set       (and bp (BP _ _))))
+       (basic-set-add a-basic-set bp))
+      ((list basic-set1 basic-set2) (basic-set-union basic-set1 basic-set2))))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Callers Set
+  (define (similar-callee? bp1 bp2)
+    (match (list bp1 bp2)
+      ((list (BP _ callee1) (BP _ callee2))
+       (state-similar? callee1 callee2))))
+
+  (define (callers-hash-code bp)
+    (match-define (BP _ callee) bp)
+    (equal-hash-code callee))
+
+  (define (get-callers Callers open)
+    (match (set-get-similar Callers (BP #f open))
+      ((some callers) (if (basic-set? callers) callers (basic-set callers)))
+      ((none)         (basic-set))))
+
+  (define empty-Callers-set (set basic-set-union/singletons-are-sets
+                                 equal?
+                                 similar-callee?
+                                 callers-hash-code))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Summaries Set
+  (define (similar-open? bp1 bp2)
+    (match (list bp1 bp2)
+      ((list (BP open1 _) (BP open2 _))
+       (state-similar? open1 open2))))
+
+  (define (summaries-hash-code bp)
+    (match-define (BP open _) bp)
+    (equal-hash-code open))
+
+  (define (get-summaries Summaries open)
+    (match (set-get-similar Summaries (BP open #f))
+      ((some similar-summaries)
+       (some (for/fold ([gte-summaries (basic-set)])
+                       ([summary (if (basic-set? similar-summaries) similar-summaries (basic-set similar-summaries))]
+                        #:when (match summary
+                                 ((BP open2 _) (gte open2 open))))
+               (basic-set-add gte-summaries summary))))
+      ((none) (none))))
+
+  (define empty-Summaries-set (set basic-set-union/singletons-are-sets
+                                   equal?
+                                   similar-open?
+                                   summaries-hash-code))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
   (define (set-add/fv s bp)
-    (match-define (BP open node) bp)
-
-    (let ((set-bp (for/first ([bp~ s]
-                              #:when (bp-similar? bp~ bp))
-                    bp~)))
-      (cond [(false? set-bp) (set-add s bp)]
-            [(bp-gte set-bp bp) s]
-            [else (set-add (set-remove/similar s set-bp)
-                           (bp-join set-bp bp))])))
+    (set-add s bp))
 
   (define (loop W Paths Summaries Callers)
-    (if (set-empty? W)
-        Paths
-        (let-values (((task W) (set-get-one/rest W)))
-          (when (> debug 0) (printf "investigating: ~a\n" task))
-          (match task
-            ((BP open (? close? close))
-             (let-values (((W Paths)
+    (match (set-get-one/rest W)
+      ((none) Paths)
+      ((some (list task W))
+       (when (> debug 0) (printf "investigating: ~a\n" task))
+       (match task
+         ((BP open (? close? close))
+          (let-values (((W Paths)
+                        (for/fold ([W W]
+                                   [Paths Paths])
+                                  ([call (get-callers Callers open)])
+                          (match call
+                            ((BP grandfather-open _)
+                             (PropagateAcross grandfather-open open close
+                                              W Paths))))))
+
+            (loop W Paths (set-add Summaries task) Callers)))
+         ((BP open1 (? open? open2))
+          (let-values (((W Paths)
+                        (match (get-summaries Summaries open2)
+                          ((none) (propagate-loop open2 (NextStates/Flow open2)
+                                                  W Paths))
+                          ((some relevant-summaries)
                            (for/fold ([W W]
                                       [Paths Paths])
-                                     ([call Callers]
-                                      #:when (match call
-                                               ((BP gf open~)
-                                                (state-similar? open open~))))
-                             (match call
-                               ((BP grandfather-open _)
-                                (PropagateAcross grandfather-open open close
-                                                 W Paths))))))
-
-               (loop W Paths (set-add/fv Summaries task) Callers)))
-            ((BP open1 (? open? open2))
-             (let-values (((W Paths)
-                           (let ((relevant-summaries
-                                  (for/fold ([relevant-summaries empty-bp-set])
-                                      ([summary Summaries]
-                                       #:when (match summary
-                                                ((BP open~ close~)
-                                                 (gte open~ open2))))
-                                    (set-add relevant-summaries summary))))
-                             (if (set-empty? relevant-summaries)
-                                 (propagate-loop open2 (NextStates/Flow open2)
-                                                 W Paths)
-                                 (for/fold ([W W]
-                                            [Paths Paths])
                                      ([summary relevant-summaries])
-                                   (match summary
-                                     ((BP open~ close~)
-                                      (PropagateAcross open1 open~ close~ W Paths))))))))
-               (loop W Paths Summaries (set-add/fv Callers task))))
-            ((BP open node)
-             (let-values (((W Paths)
-                           (propagate-loop open (NextStates/Flow node)
-                                           W Paths)))
-               (loop W Paths Summaries Callers)))))))
+                             (match summary
+                               ((BP open~ close~)
+                                (PropagateAcross open1 open~ close~ W Paths))))))))
+            (loop W Paths Summaries (set-add Callers task))))
+         ((BP open node)
+          (let-values (((W Paths)
+                        (propagate-loop open (NextStates/Flow node)
+                                        W Paths)))
+            (loop W Paths Summaries Callers)))))))
 
   ;; Propogate : OpenFState FState W Paths -> W Paths
   (define (Propagate open node W Paths)
-    (define bp (BP open node))
-
-    (let ((set-bp (for/first ([bp~ Paths]
-                              #:when (bp-similar? bp~ bp))
-                    bp~)))
-      (cond [(false? set-bp)    (values (set-add W bp) (set-add Paths bp))]
-            [(bp-gte set-bp bp) (values W Paths)]
-            [else (let ((joined-bp (bp-join set-bp bp)))
-                    (values (set-add (set-remove/similar W set-bp) joined-bp)
-                            (set-add (set-remove/similar Paths set-bp) joined-bp)))])))
+    (let ((bp (BP open node)))
+      (match (set-get-similar Paths bp)
+        ((some similar-bp)
+         (if (bp-gte similar-bp bp)
+             (values W Paths)
+             (values (set-add W bp) (set-add Paths bp))))
+        (_ (values (set-add W bp) (set-add Paths bp))))))
 
   ;; PropogateAcross : FState FState State W Paths -> W Paths
   (define (PropagateAcross grandfather-open open close W Paths)
@@ -144,6 +186,6 @@
   (let-values (((W Paths)
                 (propagate-loop initial-state
                                 (NextStates/Flow initial-state)
-                                empty-bp-set
-                                empty-bp-set)))
-    (loop W Paths empty-bp-set empty-bp-set)))
+                                empty-W/Paths-set
+                                empty-W/Paths-set)))
+    (loop W Paths empty-Summaries-set empty-Callers-set)))
